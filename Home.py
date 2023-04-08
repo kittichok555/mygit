c35e4d789291d47ae03461ed03242b019944d89c
kittichok555 <130206989+kittichok555@users.noreply.github.com>
2023-04-08 13:51:23 +0700

    .

--- Home.py
+++ Home.py
@@ -7,5 +7,14 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 #st.header("Kittichok555555555") 
-st.image("./pic/bn.jpg")
-st.image("./pic/my.jpg")
+#st.image("./pic/bn.jpg")
+#st.image("./pic/my.jpg")
+
+html_8 = """
+<div style="background-color:#990066;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
