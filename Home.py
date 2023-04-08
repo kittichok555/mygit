2baee8faf852f8205c7b7d082dcb71e98b743392
kittichok555 <130206989+kittichok555@users.noreply.github.com>
2023-04-08 13:49:11 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,5 +7,5 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 #st.header("Kittichok555555555") 
-st.image("./pic/bn.jng")
-st.image("./pic/my.jng")
+st.image("./pic/bn.jpg")
+st.image("./pic/my.jpg")
