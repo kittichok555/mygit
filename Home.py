e71c0e7e5bc4052ae689c427d9da6c6ea7c3c8ea
kittichok555 <130206989+kittichok555@users.noreply.github.com>
2023-04-08 14:05:23 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,6 +20,6 @@ st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
 
 dt = pd.read_csv("./data/iris.csv")
-st.markdown("<center>")
+st.markdown("<center>",unsafe_allow_html=True)
 st.write(dt.head(10))
-st.markdown("</center>")
+st.markdown("</center>",unsafe_allow_html=True)
